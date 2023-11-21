e85ffc881cc766dca1d1ee11ed20f838f610cc8c
zlshames <zlshames@gmail.com>
2023-11-21 16:17:13 -0500

    feat: ability to customize port via env args

--- src/privateGPT.py
+++ src/privateGPT.py
@@ -1,3 +1,5 @@
+import os
+
 from dotenv import load_dotenv
 from lib.flask import app
 from lib.flask.routes import init_routes
@@ -6,4 +8,6 @@ load_dotenv()
 
 if __name__ == "__main__":
     init_routes()
-    app.run(host="0.0.0.0", debug=True)
+
+    port = int(os.environ.get('PORT')) or 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
