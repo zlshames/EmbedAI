a9e2c1f1bba57af8b143f1cfc34d0051b72ec7bb
zlshames <zlshames@gmail.com>
2023-11-22 11:06:42 -0500

    configurable port and debug

--- src/privateGPT.py
+++ src/privateGPT.py
@@ -9,5 +9,6 @@ load_dotenv()
 if __name__ == "__main__":
     init_routes()
 
-    port = int(os.environ.get('PORT')) or 5000
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get('PORT') or '5000') or 5000
+    env = os.environ.get('ENV') or 'production'
+    app.run(host="0.0.0.0", port=port, debug=True if env == 'development' else False)
